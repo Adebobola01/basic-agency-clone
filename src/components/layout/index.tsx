33f967ce9825c0cb78f2cbe73adf5c4a8bd32e5c
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-07-16 15:42:38 +0100

    added layout and navbar

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./layout.module.scss";
+import Navbar from "../navbar";
+
+
+const Layout = ()=>{
+
+    return (
+        <Navbar/>
+    )
+}
+
+export default Layout;
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./navbar.module.scss";
+
+const Navbar = ()=>{
+
+    return(
+        <header>
+            <p>header</p>
+        </header>
+    )
+}
+
+export default Navbar;
