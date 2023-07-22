6594812082f397c31e8eeb4d9a761d4803faad17
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-07-22 23:57:24 +0100

    added footer content

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -17,6 +17,23 @@ const Footer = ()=>{
                         <a href="mailto:biz@basicagency.com">biz@basicagency.com</a>
                     </p>
                 </div>
+                <div className={styles.footer_row} >
+                    <div className={styles.footer_col} >
+                        <h6>STAY IN THE KNOW</h6>
+                        <form>
+                            <input type="email" placeholder="Email Address" required />
+                            <button type="submit"  >
+                                <figure>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 17 17">
+                                        <path d="M.1 7.5h14v2H.1z"></path>
+                                        <path d="m8.4 0 8.5 8.5-1.4 1.4L7 1.4 8.4 0z"></path>
+                                        <path d="m7 15.6 8.5-8.5 1.4 1.4L8.4 17 7 15.6z"></path>
+                                    </svg>
+                                </figure>
+                            </button>
+                        </form>
+                    </div>
+                </div>
             </div>
         </footer>
     )
