78c60f980ae9e11ea7ee57bac3ca788b033298d4
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-07-23 01:09:49 +0100

    added footer content

---
+++ src/components/footer/col/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+
+type colType = {
+    title: string,
+    links: Array<string>,
+
+}
+
+const Col = (props:colType)=>{
+    const linkContent = props.links.map(it => <li>{it}</li>)
+
+    return(
+        <div className={styles.col} >
+            <h6>{props.title}</h6>
+            <ul>
+                {linkContent}
+            </ul>
+        </div>
+    )
+}
+
+export default Col;
