41ffb10436e4970eaf7c1210d189a8bba8a3c712
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-07-25 20:49:15 +0100

    hero

--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,13 +1,26 @@
 import React from "react";
-import heroVid from "../../assets/images/4c749533161fc77c899a376ec6cd6da38973772f.mp4"
+import { useState } from "react";
+import heroVid from "../../assets/images/4c749533161fc77c899a376ec6cd6da38973772f.mp4";
+import heroGif from "../../assets/images/c6fb986a862cbe643c40cbdd0318ebc495efb187.mp4";
 import styles from "./index.module.scss";
+import VideoPlayer from "../videoPlayer";
 const Hero = ()=>{
+    const [hero, setHero] = useState(false);
+
+    const heroHandler = ()=>{
+        setHero(!hero);
+        console.log(hero)
+    }
+
+
+    let content = (hero ? <VideoPlayer mute={false} video={heroVid}/>
+        : <VideoPlayer video={heroGif} mute={true} />)
+
 
     return (
         <section className={styles.hero} >
-            <video autoPlay={true} controls>
-                <source src={heroVid} type="video/mp4" />
-            </video>
+            <div className={styles.cover} onClick={heroHandler} ></div>
+            {content}
         </section>
     )
 }
---
+++ src/components/videoPlayer/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const VideoPlayer = (props: any)=>{
+
+    return (
+        <video autoPlay controls={true} muted={props.mute} loop={true} className={styles.videoPlayer} onClick={props.handler} >
+            <source src={props.video} type="video/mp4" />
+        </video>
+    )
+}
+
+export default VideoPlayer;
