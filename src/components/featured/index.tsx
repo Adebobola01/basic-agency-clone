a2a05c1a0a2603f80d454e0de01e0c18c612685c
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-08-10 04:02:46 +0100

    fix: featured section

--- src/components/featured/index.tsx
+++ src/components/featured/index.tsx
@@ -24,11 +24,11 @@ const Featured = ()=>{
                 featured engagements
             </h2>
                 <div className={styles.featured_engagements} >
-                    <EngagamentItem imageLink={googleImage} title="google" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership " />
-                    <EngagamentItem imageLink={kfcImage} title="kfc" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership " />
-                    <EngagamentItem imageLink={wilsonImage} title="wilson" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership " />
-                    <EngagamentItem imageLink={atImage} title="at&t" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership " />
-                    <EngagamentItem imageLink={patagImage} title="patagonia" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership " />
+                    <EngagamentItem imageLink={googleImage} title="google" text="Our embedded partnership with Google is as deep as it gets. We’re the lead creative agency for Google Store and provide strategy, design, and prototyping to other divisions. Learn more about our partnership here" />
+                    <EngagamentItem imageLink={kfcImage} title="kfc" text="An award-winning global, digital transformation engagement spanning eCommerce, mobile app, and new drive thru experiences. Bringing KFC’s brand story to life while making it easier for customers to buy chicken. Learn more about our partnership here" />
+                    <EngagamentItem imageLink={wilsonImage} title="wilson" text="A reimagining of Wilson’s brand visual identity, and brand campaign, to support a new product drop and the launch of the first brick and mortar retail location in the brand’s 108-year history. Read our full case study here" />
+                    <EngagamentItem imageLink={atImage} title="at&t" text="Redesigning the digital flagship for the largest telecommunications company in the world. Creating frictionless paths to purchase for a wide range of consumers across a vast portfolio of products and services." />
+                    <EngagamentItem imageLink={patagImage} title="patagonia" text="Ongoing partnership providing strategy, branding, experience design, and development focused on bringing their mission and offerings to consumers through brand-led programs and platforms. Read our full case study" />
                 </div>
         </section>
     )
