d9f079de8a437b6dace8fb5e84e60c8559faeb4c
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-08-26 13:06:02 +0100

    feat: added custom cursor to hero

---
+++ src/components/cursor/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./index.module.scss";
+
+const Cursor = ()=>{
+
+
+    return(
+        <div className={styles.cursor} >
+            <span>watch reel</span>
+        </div>
+    )
+}
+
+export default Cursor;
--- src/components/hero/index.tsx
+++ src/components/hero/index.tsx
@@ -1,25 +1,71 @@
 import React from "react";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import heroVid from "../../assets/images/4c749533161fc77c899a376ec6cd6da38973772f.mp4";
 import heroGif from "../../assets/images/c6fb986a862cbe643c40cbdd0318ebc495efb187.mp4";
 import styles from "./index.module.scss";
 import VideoPlayer from "../videoPlayer";
+
+
+
+type cursorPtType = {
+    clientX: number, 
+    clientY: number
+}
+
+
 const Hero = ()=>{
-    const [hero, setHero] = useState(true);
-    const [muteVid, setmuteVid] = useState(true)
+    const [playHeroGif, setPlayHeroGif] = useState(true);
+    const [muteVid, setmuteVid] = useState(true);
+    const [clientX, setCursorX] = useState(600)
+    const [clientY, setCursorY] = useState(370)
+    const [cursorStyle, setCursorStyle] = useState({});
+    
     const heroHandler = ()=>{
-        setHero(!hero);
+        setPlayHeroGif(!playHeroGif);
         setTimeout(()=>{
-            setmuteVid(false);
+            setmuteVid(!muteVid);
         }, 1)
     }
 
 
+    useEffect(()=>{
+        cursorStyleHandler({clientX, clientY});
+        // return cursorStyleHandler({clientX: 600, clientY: 370});
+    }, [clientX, clientY])
+
+    const moveMouse = ({clientX, clientY}: cursorPtType)=>{
+        setCursorX(clientX);
+        setCursorY(clientY);
+    }
+
+    const cursorStyleHandler = ({clientX, clientY}: cursorPtType)=>{
+        setCursorStyle({
+            transform: `translate(${clientX}px, ${clientY}px)`,
+            display: playHeroGif ? "flex" : "none"
+        });
+        console.log(cursorStyle);
+    }
+
+    // console.log(clientX, clientY)
+
+
     return (
-        <section className={styles.hero} >
+        <section className={styles.hero} onMouseMove={moveMouse} >
+
+            <div className={styles.cursor} style={cursorStyle} >
+                <div className={styles.cursor_top} >
+                    <p>watch <br/> 
+                    reel</p>
+                </div>
+                <div className={styles.cursor_down} >
+                    <p>BASIC/DEPT®</p>
+                    <p>2010-*</p>
+                </div>
+            </div>
+
             <div className={styles.cover} onClick={heroHandler} ></div>
-            <VideoPlayer mute={muteVid} video={heroVid} loop={false} show={!hero} />
-            <VideoPlayer video={heroGif} mute={true} show={hero} loop={true} />
+            <VideoPlayer mute={muteVid} video={heroVid} loop={false} show={!playHeroGif} />
+            <VideoPlayer video={heroGif} mute={true} show={playHeroGif} loop={true} />
         </section>
     )
 }
