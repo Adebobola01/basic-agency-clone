b627f5ce1811af463d8b7856e3bbeec75346ac93
Adebobola Oyedunmade <m.oyedunmade@gmail.com>
2023-08-30 22:36:26 +0100

    fix: fixed videoPlayer

--- src/components/videoPlayer/index.tsx
+++ src/components/videoPlayer/index.tsx
@@ -1,21 +1,30 @@
-import React from "react";
+import React, {useRef} from "react";
 import styles from "./index.module.scss";
 
 type videoType = {
     mute: boolean,
     video: any,
     loop?: boolean,
-    show?: boolean 
+    show?: boolean,
+    pause?: boolean,
 }
 
 const VideoPlayer = (props: videoType)=>{
+    const vidRef = useRef();
+
+    if(!props.show){
+        vidRef.current?.pause();
+    }else{
+        vidRef.current?.play();
+    }
+
 
     const style = {
         display: props.show ? "block" : "none"
     }
 
     return (
-        <video autoPlay controls={true} playsInline muted={props.mute} loop={props.loop} className={styles.videoPlayer} style={style} >
+        <video autoPlay controls={false} playsInline muted={props.mute}  loop={props.loop} ref={vidRef} className={styles.videoPlayer} style={style} >
             <source src={props.video} type="video/mp4" />
         </video>
     )
